ed8ea37f968b18d89fb8a85e1c628f937e7190f4
StJohn Piano <stjohnpiano@gmail.com>
2024-03-14 13:16:38 +0100

    Upgrade actions and remove commented lines

--- tests/db/test_1.py
+++ tests/db/test_1.py
@@ -15,12 +15,6 @@ DB_USER = os.environ.get("DB_USER")
 DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
-#DB_NAME = os.environ.get("DB_NAME")
-
-# DB_USER = "postgres"
-# DB_PASSWORD = "postgres"
-# DB_HOST = "localhost"
-# DB_PORT = "5432"
 
 TEST_DB_NAME = "test_db"
 
